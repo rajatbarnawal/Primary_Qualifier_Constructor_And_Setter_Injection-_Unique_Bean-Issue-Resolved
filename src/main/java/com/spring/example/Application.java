2598c47538a46ccc30e3fa75b3df0d80ac8dc969
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2019-12-28 16:02:14 +0545

    Update Application.java

--- src/main/java/com/spring/example/Application.java
+++ src/main/java/com/spring/example/Application.java
@@ -8,12 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Ignore Kyun Mar raha ");
-=======
-		System.out.println("I love Anugya Shah ");
-		
->>>>>>> multiply
+	System.out.println("You Should have trusted me to finish the job");
 		SpringApplication.run(Application.class, args);
 	}
 
